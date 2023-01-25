39d14b660f2b074b5d309ccc5e3ad51cd3f8a05e
daniahnaf22 <ahnaffalihdani@gmail.com>
2023-01-25 22:32:32 +0700

    edit nav

--- resources/views/home/nav.blade.php
+++ resources/views/home/nav.blade.php
@@ -1,16 +1,22 @@
 <nav class="navbar navbar-expand-md navbar-white fixed-top bg-white">
-            <div class="container">
-                <a class="navbar-brand" href="#">Carousel</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link " aria-current="page" href="/login">Login</a>
-                        </li>
-                    </ul>
+    <div class="container">
+        <a class="navbar-brand" href="/">Beasiswa PNP</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ms-auto">
+                <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 link-dark">Home</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
+                </ul>
+
+                <div class="col-md-3 text-end">
+                    <a href="/login" class="btn btn-outline-primary me-2">Login</a>
                 </div>
-            </div>
-        </nav>
+            </ul>
+        </div>
+    </div>
+</nav>
